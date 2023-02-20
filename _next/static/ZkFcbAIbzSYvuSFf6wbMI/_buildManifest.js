ac7e23551c10274479c9c8930f87d7922ad3401c
davidebrognoli <davidebrognoli@users.noreply.github.com>
2023-02-20 20:44:40 +0000

    deploy: 46b4158f11744852e4f1e928c4b1a954753712cc

---
+++ _next/static/ZkFcbAIbzSYvuSFf6wbMI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/94bd8b4675deb6e7.css","static/chunks/pages/index-5eaab1232fdbc1ba.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
